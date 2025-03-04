3670c1574a6b8fe82962d1e530db73dc56e46539
1hp <keshasuper396@gmail.com>
2025-03-05 02:22:27 +0300

    Навигация

--- Classes and WPF 9/Wpf/View/Pages/Page16.xaml.cs
+++ Classes and WPF 9/Wpf/View/Pages/Page16.xaml.cs
@@ -12,17 +12,36 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Classes_and_WPF_9.Classes;
 
 namespace Wpf.View.Pages
 {
-    /// <summary>
-    /// Логика взаимодействия для Page16.xaml
-    /// </summary>
     public partial class Page16 : Page
     {
         public Page16()
         {
             InitializeComponent();
+        }
+
+        private void BtnTask16_Click(object sender, RoutedEventArgs e)
+        {
+            //double G = Math.Exp(2 * Convert.ToDouble(TbY.Text)) + Math.Sin(Convert.ToDouble(Tbf.Text)) / Math.Log10(3.8 * Convert.ToDouble(TbY.Text) + Convert.ToDouble(Tbf.Text));
+             Class1 = new Class1();
+
+            Calculator16[] calculator16s =
+                {
+                    new Calculator16(3, 5, 4),
+
+                    new Calculator16(3, 8, 11),
+
+                    new Calculator16(9, 9, 9)
+                };
+            foreach (var calc in calculator16s)
+            {
+                string result = calc.CalculateA();
+                MessageBox.Show($"Результат: {result}");
+            }
+
         }
     }
 }
